ad282a0910f3be3796d33a9f38f02fb0b417743a
Eliecer Diaz Arrieta <ejmenrique97@gmail.com>
2024-03-11 11:37:37 -0500

    add section experience

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,22 +1,75 @@
 import React from 'react'
 import Section from '../Section'
-
+import { experience } from '../../../cv.json'
+import styled from 'styled-components';
 function Experience() {
     return (
         <Section title="Experiencia laboral">
-            <p>
-                Aunque carezco de experiencia laboral formal, mi dedicación hacia el desarrollo web y mis habilidades en HTML, CSS y JavaScript son respaldadas por proyectos personales que demuestran mi destreza técnica. Mi entusiasmo y disposición para aprender se traducen en una sólida comprensión de React, una herramienta que utilizo con confianza para crear interfaces dinámicas y atractivas.
-            </p>
-            <br />
-            <p>
-                Soy un aprendiz diligente, comprometido con el crecimiento continuo y la adopción de las últimas tendencias en desarrollo frontend. Mi mentalidad proactiva y mi habilidad para resolver problemas creativamente me convierten en un candidato entusiasta y capaz, ansioso por contribuir a un equipo innovador.
-            </p>
-            <br />
-            <p>
-                Aunque mi experiencia sea limitada, mi conocimiento y pasión por la tecnología me posicionan como un valioso recurso, listo para enfrentar desafíos y aportar a un equipo de desarrollo frontend con energía y determinación.
-            </p>
+            <ul>
+                {experience.map(({ company, startDate, endDate, area, functions }) => {
+                    const years = `${startDate} - ${endDate}`;
+                    return (
+                        <li key={company}>
+                            <Article>
+                                <header>
+                                    <div>
+                                        <h3>{company}</h3>
+                                    </div>
+                                    <span>{years}</span>
+                                </header>
+                                <footer>
+                                    <span>{area}</span>
+                                    <ul>
+                                        {
+                                            functions.map((func, index) => (
+                                                <li key={index}>{func}</li>
+                                            ))
+                                        }
+                                    </ul>
+                                </footer>
+                            </Article>
+                        </li>
+                    );
+                })}
+            </ul>
         </Section>
     )
 }
 
 export default Experience
+const Article = styled.article`
+    header{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        
+        div{
+            width: 70vh;
+
+        }
+
+        @media (max-width: 700px) {
+            span{
+                font-size: 1em;
+            }
+        }
+    }
+
+    footer{
+        ul{
+            width: 70vh;
+            li{
+                list-style: disc;
+                margin-left: 2em;
+                text-align: justify;
+                padding-top: 1em;
+            }
+        }
+
+        @media (max-width: 700px) {
+            span{
+                font-size: 1em;
+            }
+        }
+    }
+`
